e5e05704617af88419d151a72eccedea0675c606
Winston Gao <leon20130301@hotmail.com>
2024-12-26 20:26:14 +1100

    Update app.py

--- app.py
+++ app.py
@@ -6,17 +6,15 @@ import uuid
 
 # Get config from environment variables
 agent_id = os.environ.get("BEDROCK_AGENT_ID")
-agent_alias_id = os.environ.get("BEDROCK_AGENT_ALIAS_ID", "TSTALIASID")  # TSTALIASID is the default test alias ID
+agent_alias_id = os.environ.get("BEDROCK_AGENT_ALIAS_ID", "TSTALIASID") # TSTALIASID is the default test alias ID
 ui_title = os.environ.get("BEDROCK_AGENT_TEST_UI_TITLE", "Agents for Amazon Bedrock Test UI")
 ui_icon = os.environ.get("BEDROCK_AGENT_TEST_UI_ICON")
-
 
 def init_state():
     st.session_state.session_id = str(uuid.uuid4())
     st.session_state.messages = []
     st.session_state.citations = []
     st.session_state.trace = {}
-
 
 # General page configuration and initialization
 st.set_page_config(page_title=ui_title, page_icon=ui_icon, layout="wide")
@@ -43,39 +41,35 @@ if prompt := st.chat_input():
     with st.chat_message("assistant"):
         placeholder = st.empty()
         placeholder.markdown("...")
-        try:
-            response = bedrock_agent_runtime.invoke_agent(
-                agent_id,
-                agent_alias_id,
-                st.session_state.session_id,
-                prompt
-            )
-            output_text = response["output_text"]
+        response = bedrock_agent_runtime.invoke_agent(
+            agent_id,
+            agent_alias_id,
+            st.session_state.session_id,
+            prompt
+        )
+        output_text = response["output_text"]
 
-            # Add citations
-            if len(response["citations"]) > 0:
-                citation_num = 1
-                num_citation_chars = 0
-                citation_locs = ""
-                for citation in response["citations"]:
-                    if "retrievedReferences" in citation:
-                        end_span = citation["generatedResponsePart"]["textResponsePart"]["span"]["end"] + 1
-                        for retrieved_ref in citation["retrievedReferences"]:
-                            citation_marker = f"[{citation_num}]"
-                            output_text = output_text[:end_span + num_citation_chars] + citation_marker + output_text[end_span + num_citation_chars:]
-                            citation_locs = citation_locs + "\n<br>" + citation_marker + " " + retrieved_ref["location"]["s3Location"]["uri"]
-                            citation_num += 1
-                            num_citation_chars += len(citation_marker)
-                        output_text = output_text[:end_span + num_citation_chars] + "\n" + output_text[end_span + num_citation_chars:]
-                        num_citation_chars += 1
-                output_text += "\n" + citation_locs
+        # Add citations
+        if len(response["citations"]) > 0:
+            citation_num = 1
+            num_citation_chars = 0
+            citation_locs = ""
+            for citation in response["citations"]:
+                end_span = citation["generatedResponsePart"]["textResponsePart"]["span"]["end"] + 1
+                for retrieved_ref in citation["retrievedReferences"]:
+                    citation_marker = f"[{citation_num}]"
+                    output_text = output_text[:end_span + num_citation_chars] + citation_marker + output_text[end_span + num_citation_chars:]
+                    citation_locs = citation_locs + "\n<br>" + citation_marker + " " + retrieved_ref["location"]["s3Location"]["uri"]
+                    citation_num = citation_num + 1
+                    num_citation_chars = num_citation_chars + len(citation_marker)
+                output_text = output_text[:end_span + num_citation_chars] + "\n" + output_text[end_span + num_citation_chars:]
+                num_citation_chars = num_citation_chars + 1
+            output_text = output_text + "\n" + citation_locs
 
-            placeholder.markdown(output_text, unsafe_allow_html=True)
-            st.session_state.messages.append({"role": "assistant", "content": output_text})
-            st.session_state.citations = response["citations"]
-            st.session_state.trace = response["trace"]
-        except Exception as e:
-            placeholder.markdown(f"Error occurred while invoking Bedrock Agent.\n\nError: {e}")
+        placeholder.markdown(output_text, unsafe_allow_html=True)
+        st.session_state.messages.append({"role": "assistant", "content": output_text})
+        st.session_state.citations = response["citations"]
+        st.session_state.trace = response["trace"]
 
 trace_types_map = {
     "Pre-Processing": ["preGuardrailTrace", "preProcessingTrace"],
@@ -93,7 +87,7 @@ trace_info_types_map = {
 with st.sidebar:
     st.title("Trace")
 
-    # Show each trace type in separate sections
+    # Show each trace types in separate sections
     step_num = 1
     for trace_type_header in trace_types_map:
         st.subheader(trace_type_header)
@@ -131,7 +125,7 @@ with st.sidebar:
                         for trace in trace_steps[trace_id]:
                             trace_str = json.dumps(trace, indent=2)
                             st.code(trace_str, language="json", line_numbers=trace_str.count("\n"))
-                    step_num += 1
+                    step_num = step_num + 1
         if not has_trace:
             st.text("None")
 
@@ -145,7 +139,7 @@ with st.sidebar:
                         "generatedResponsePart": citation["generatedResponsePart"],
                         "retrievedReference": citation["retrievedReferences"][retrieved_ref_num]
                     }, indent=2)
-                    st.code(citation_str, language="json", line_numbers=citation_str.count("\n"))
-                citation_num += 1
+                    st.code(citation_str, language="json", line_numbers=trace_str.count("\n"))
+                citation_num = citation_num + 1
     else:
         st.text("None")
